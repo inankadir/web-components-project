1e536d8d68ed83411c19d3b6e4c8eab5703d6857
kadir <idia_2008@hotmail.com>
2023-02-03 00:45:17 +0300

    add new file

---
+++ components/appHeader/appHeader.js
@@ -0,0 +1,20 @@
+const headerTemplate = document.createElement("template");
+headerTemplate.innerHTML = `
+<style>
+@import url('http://${location.host}/components/appHeader/appHeader.css')
+</style>
+<header>
+Web Components Movie App
+</header>
+`;
+
+
+class AppHeader extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+        this.shadowRoot.appendChild(headerTemplate.content.cloneNode(true));
+    }
+}
+
+window.customElements.define("app-header", AppHeader);
